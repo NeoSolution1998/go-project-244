a7853547d3c639b8802292cb8a6098028490e75a
NeoSolution <neosolution1998@gmail.com>
2026-01-06 20:38:09 +0300

    Add --format option

---
+++ Makefile
@@ -0,0 +1,2 @@
+run:
+	go run cmd/gendiff/main.go $(ARGS)
--- cmd/gendiff/main.go
+++ cmd/gendiff/main.go
@@ -11,6 +11,14 @@ func main() {
 	app := &cli.Command{
 		Name:  "gendiff",
 		Usage: "Compares two configuration files and shows a difference.",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "format",
+				Aliases: []string{"f"},
+				Usage:   "output format",
+				Value:   "stylish",
+			},
+		},
 	}
 
 	app.Run(context.Background(), os.Args)
